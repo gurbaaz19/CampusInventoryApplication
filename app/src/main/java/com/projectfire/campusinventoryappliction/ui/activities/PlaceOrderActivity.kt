657ffae1721fe7293301d80968a6644d65b1cca6
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-11-28 14:23:47 +0530

    Changes

--- app/src/main/java/com/projectfire/campusinventoryappliction/ui/activities/PlaceOrderActivity.kt
+++ app/src/main/java/com/projectfire/campusinventoryappliction/ui/activities/PlaceOrderActivity.kt
@@ -63,7 +63,7 @@ class PlaceOrderActivity : BaseActivity() {
 
     fun userDetailsSuccess(user: User) {
 
-        tv_checkout_address.text = "${user.address}, ${user.bitsID}"
+        tv_checkout_address.text = "${user.address}, ${user.bits_id}"
         tv_checkout_full_name.text = "${user.firstName} ${user.lastName}"
         tv_checkout_mobile_number.text = user.mobile.toString()
 
@@ -150,7 +150,7 @@ class PlaceOrderActivity : BaseActivity() {
             "${mUser.firstName} ${mUser.lastName}",
             mCartList,
             mUser.address,
-            mUser.bitsID,
+            mUser.bits_id,
             mUser.mobile.toString(),
             "Order of ${mUser.firstName} at ${orderDate}",
             mCartList[0].image,
